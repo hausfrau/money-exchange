c4f41704b54fb2bd5ab48e5771b6022542987362
hausfrau <hausfrau.sunlight@gmail.com>
2019-03-15 12:41:27 +0300

    feat: complete Money Exchange task

--- src/index.js
+++ src/index.js
@@ -1,5 +1,35 @@
-// PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+    if (currency < 1) {
+        return {};
+    }
+
+    if (currency > 10000) {
+        return {
+            error: "You are rich, my friend! We don't have so much coins for exchange"
+        };
+    }
+
+    const returnObject = {};
+    const currencyValues = [50, 25, 10, 5, 1];
+    const associativeCurrencyValue = {
+        50: "H",
+        25: "Q",
+        10: "D",
+        5: "N",
+        1: "P"
+    };
+
+    currencyValues.reduce(function (balance, currencyValueItem) {
+        if (currencyValueItem <= balance) {
+            let piece = balance / currencyValueItem >> 0;
+            let value = associativeCurrencyValue[currencyValueItem];
+            returnObject[value] ? returnObject[value] += piece : returnObject[value] = piece;
+            return balance - (currencyValueItem * piece);
+        } else {
+            return balance;
+        }
+
+    }, currency);
+
+    return returnObject;
 }
